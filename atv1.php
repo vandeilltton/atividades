96e1c21ab1f91d4ef2771a26f50e2467ac27da01
Vandeilltton <vandeilltton@gmail.com>
2020-02-14 13:58:31 -0300

    update
    vc coloca o nome que preferir aqui e acima

---
+++ atv1.php
@@ -0,0 +1,7 @@
+<?php
+
+$i = 0;
+while($i <= 100){
+	echo "É possivel repetir várias linhas no PHP. Esta linha foi repetida ".$i." vezes <br>";
+	$i++;
+}
---
+++ atv2.php
@@ -0,0 +1,7 @@
+<?php
+
+$i = 0;
+while($i <= $_GET["total"]){
+	echo "É possivel repetir várias linhas no PHP. Esta linha foi repetida ".$i." vezes <br>";
+	$i++;
+}
